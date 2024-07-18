785f4039121f9e20a72078b4a6a082c5b0b9c020
Korepanov <P.KorepanovIT@yandex.com>
2024-07-18 18:34:26 +0300

    Clean up 2

--- js-service.js
+++ js-service.js
@@ -54,10 +54,10 @@ function problem2__Validation() {
 	let true_message = "Sudoku is valid";
 	let false_message = "Sudoku is not valid";
 	let err_message1 = "Symbols beside 1-9 are ignored";
-	let input_board = null; // input_board[0-8][0-8]
-	input_board = read_sudoku();
+	let Sudoku = null; // input_board[0-8][0-8]
+	Sudoku = read_sudoku();
 	// algorithm:
-	res = isValidSudoku(input_board);
+	res = isValidSudoku(Sudoku);
 	// Print out the result;
 	//~ problem2__outputTracker.textContent = res;
 	if(res) problem2__outputTracker.textContent = true_message;
@@ -67,46 +67,35 @@ function problem2__Validation() {
 
 // Button 2: Write in site Seed Sudoku
 function problem2__GenerateSeedSudoku() {
-	// Definitions:
-	let msg_wrong_symbols = "Symbols beside 1-9 are ignored";
-	
-	// Create seed sudoku:
-	var board = seedSudoku();
-	
-	// Write in numbers in sudoku on site:
-	writeInSudoku(board);
-	//~ return board;
+	var Sudoku = seedSudoku();
+	writeInSudoku(Sudoku);
 	};
 
 // Button 3: One not safe permutations (swap cell1 and cell2)
 function problem2__PermutateSudoku() {
 	// Definitions:
-	//~ let msg_wrong_symbols = "Symbols beside 1-9 are ignored";
-	let input_board = null; // input_board[0-8][0-8]
-
+	let Sudoku = null; // input_board[0-8][0-8]
 	const i1 = Number(document.getElementsByClassName('problem2__js-input-permutations_i1')[0].value);
 	const j1 = Number(document.getElementsByClassName('problem2__js-input-permutations_j1')[0].value);
 	const i2 = Number(document.getElementsByClassName('problem2__js-input-permutations_i2')[0].value);
 	const j2 = Number(document.getElementsByClassName('problem2__js-input-permutations_j2')[0].value);
 	
 	// Create seed sudoku:
-	input_board = read_sudoku();
+	Sudoku = read_sudoku();
 	
 	// Change in sudoku: permutations between rows 4 and 5.
-	board = permutation(input_board, i1,j1, i2,j2);
+	permutation(Sudoku, i1,j1, i2,j2);
 	
 	// Write in numbers in sudoku on site:
-	writeInSudoku(input_board);
+	writeInSudoku(Sudoku);
 	};
 
 // Button 4: Safe permutation (swap row1 and row2)
 function problem2__PermutateSudokuRows() {
 	// Definitions:
-	//~ let msg_wrong_symbols = "Symbols beside 1-9 are ignored";
 	let Sudoku = null; // Sudoku[0-8][0-8]
 	const row1 = Number(document.getElementsByClassName('problem2__js-input-permutate_row1')[0].value);
 	const row2 = Number(document.getElementsByClassName('problem2__js-input-permutate_row2')[0].value);
-	//~ alert(document.getElementsByClassName('problem2__js-input-permutate_row1')[0]);
 	
 	// Create seed sudoku:
 	Sudoku = read_sudoku();
@@ -127,71 +116,28 @@ function problem2__PermutateSudokuRows() {
 // 1. Read from the form sudoku.
 // 2. Write in the form sudoku.
 function read_sudoku() {
-	let board = create_array();
+	let SudokuBoard = create_array();
 	let msg_wrong_symbols = "Symbols beside 1-9 are ignored";
-	//~ alert(board[8][8])
     for( let i = 0; i < 9; i++)
 		for( let j = 0; j < 9; j++) {
 			input_str = problem2__inputTracker[i*9 + j].value;
 			if(input_str) {
 				inp_num = Number(input_str);
 				if(!inp_num) {
-					board[i][j]='.';
+					SudokuBoard[i][j]='.';
 					alert(msg_wrong_symbols);
 					}
-				else board[i][j] = input_str;
+				else SudokuBoard[i][j] = input_str;
 				};
 			};
-	return board;
+	return SudokuBoard;
 	};
 
-function writeInSudoku(board) {
+function writeInSudoku(SudokuBoard) {
     for( let i = 0; i < 9; i++)
 		for( let j = 0; j < 9; j++) {
-			problem2__inputTracker[i*9 + j].value = board[i][j];
+			problem2__inputTracker[i*9 + j].value = SudokuBoard[i][j];
 			};
-	};
-
-
-
-
-
-
-
-
-
-
-
-
-
-// OLD code: to delete
-function problem2__PermutateSudoku_2() {
-	// Definitions:
-	//~ let msg_wrong_symbols = "Symbols beside 1-9 are ignored";
-	let input_board = null; // input_board[0-8][0-8]
-
-	// Create seed sudoku:
-	input_board = read_sudoku();
-	
-	// Make permutations over seed sudoku:
-	//~ board = permutation(board, 0,0, 8,8);
-	//~ alert(board[0][0]);
-	
-	// No change in the end: permutations between quadrants.
-	//~ board = permutation(input_board, 7,3, 6,4);
-	//~ board = permutation(input_board, 7,1, 4,6);
-	//~ board = permutation(input_board, 7,1, 7,3);
-	//~ board = permutation(input_board, 6,4, 4,6);
-	
-	// Change in sudoku: permutations between rows 4 and 5.
-	board = permutation(input_board, 4,6, 5,6);
-	board = permutation(input_board, 4,9, 5,9);
-	//~ board = permutation(input_board, 7,1, 7,3);
-	//~ board = permutation(input_board, 6,4, 4,6);
-	//~ alert(board[0][0]);
-	// Write in numbers in sudoku on site:
-	writeInSudoku(input_board);
-	//~ return board;
 	};
 
 
