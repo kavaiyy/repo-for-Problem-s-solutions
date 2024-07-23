cff062bdfa8044c3f35379ff3864085f2e016ac1
Korepanov <P.KorepanovIT@yandex.com>
2024-07-23 22:29:35 +0300

    Add one algorithm in comments for future to-do

--- problems/Problem4_Sudoku.js
+++ problems/Problem4_Sudoku.js
@@ -187,3 +187,49 @@ function CheckIfOnlyOneSolution(board, i_element, j_element) {
 	return 0;
 	};
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Python algorith from leetcode:
+//~ class Solution:
+    //~ def solveSudoku(self, board: List[List[str]]) -> None:
+        //~ """
+        //~ Do not return anything, modify board in-place instead.
+        //~ """
+        //~ def check(i,j,board,num):
+            //~ if(num in board[i]):return False
+            //~ if(num in [board[row][j] for row in range(9)]):return False
+            //~ for r in range((i//3)*3,((i//3)*3)+3):
+                //~ for c in range((j//3)*3,((j//3)*3)+3):
+                    //~ if(board[r][c]==num):return False
+            //~ return True
+
+        //~ def rec(board,i,j):
+            //~ if(j==9):
+                //~ i+=1;j=0
+            //~ if(i==9):
+                //~ return True
+            //~ if(board[i][j]=="."):
+                //~ for num in range(1,10):
+                    //~ if(check(i,j,board,str(num))):
+                        //~ board[i][j]=str(num)
+                        //~ if(rec(board,i,j+1)):return True
+                        //~ board[i][j]="."
+                //~ return False
+            //~ else:
+                //~ return rec(board,i,j+1)
+        //~ rec(board,0,0)
+        //~ return board
